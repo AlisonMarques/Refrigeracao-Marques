081f2ef834fc25a29c01f8ce06b267e89e33df11
AlisonMarques <alison-marques@outlook.com.br>
2021-02-01 17:40:31 -0300

    Desenvolvido a estrutura do component Home.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 import Header from '../components/Header'
 import Menu from '../components/Menu'
+import Home1 from '../components/Home'
 
 export default function Home() {
   return (
     <div className="super_container">
       <Header />
       <Menu />
+      <Home1 />
     </div>
   )
 }
